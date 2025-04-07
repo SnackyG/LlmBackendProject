2fa6c66e2386e5dd9f02c75f8cefe4f0aa58bdfa
SnackyG <Klaus.e.gregersen@gmail.com>
2025-04-07 09:35:38 +0200

    pew pew

---
+++ src/main/java/dk_kea/llmbackendproject/LlmBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package dk_kea.llmbackendproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmBackendProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlmBackendProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk_kea/llmbackendproject/LlmBackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dk_kea.llmbackendproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmBackendProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
